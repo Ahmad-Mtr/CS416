6c5fedee5ed2b8cd8b198addfd56a7e889126cec
Ahmad-Mtr <ahmadmtr@protonmail.com>
2024-04-01 14:55:27 +0300

    update lab5

--- lab5/task.c
+++ lab5/task.c
@@ -4,35 +4,37 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUF_SIZE 25
+#define BUF_SIZE 250
 int main() {
   pid_t pid;
   int f1, f2;
   off_t _offset;
   ssize_t numByteRead, numByteWritten;
   char buffer[BUF_SIZE];
-  const char *name = "Ahmad MAhmoud";
+  const char *name = "\nAhmad MAhmoud";
   const char *course = "Systems Programming";
   pid = fork();
   if (pid < 0) {
     // Error
   } else if (pid == 0) {
     f1 = open("Name.txt", O_RDWR | O_CREAT,
-              0644); // open   or create the first file
+              0644); // open or create the first file
     if (f1 == -1) {
       printf("Error opening or creating file1");
       exit(1);
     }
     numByteWritten = write(f1, course, strlen(course));
+    close(f1);
 
   } else {
     // Parent
     wait(NULL);
+    f2 = open("Name.txt", O_APPEND | O_RDWR,
+              0644); // open or create the first file
 
-    _offset = lseek(f1, 100, SEEK_END);
-    write(f1, name, strlen(name));
-    close(f1);
+    write(f2, name, strlen(name));
   }
+    close(f2);
 
   return 0;
 }
