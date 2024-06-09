52260c5be085a80574b72d2dcc2a6bb25b3b977e
Ahmad-Mtr <ahmadmtr@protonmail.com>
2024-06-09 17:38:05 +0300

    add pdf

--- _rev/main.c
+++ _rev/main.c
@@ -1,11 +1,44 @@
+
+/*
+  Create Program that triggers a system call to mkdir C in it B in it A.
+  .
+  ├── C
+  │   └── B
+  │       └── A
+  ├── task.c
+  └── they_say_this_is_exe
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
+#include <unistd.h>
 
+int main() {
 
-int main(){
-  
+  printf("Process A\n");
+  pid_t pid1;
+  // Fork Part
+  pid1 = fork();
+  if (pid1 < 0) {
+    printf("Errorr\n");
+  } else if (pid1 > 0) {
+    // Parent A
+  } else {
+    // Child B
+    pid_t pid2 = fork();
+    if (pid2 == 0) {
+      // C
+      execl("/bin/mkdir", "mkdir", "C", NULL);
+      exit(1);
+    } else {
+      // B
+      wait(NULL);
+      execl("/bin/mkdir", "mkdir", "C/B", NULL);
+      exit(1);
+    }
+  }
+  // Folder Part
+  wait(NULL);
+  execl("/bin/mkdir", "mkdir", "C/B/A", NULL);
   return 0;
 }
